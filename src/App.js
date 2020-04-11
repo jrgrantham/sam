2d3751781b64e7961752e3e8ec90bed818ed48c3
James Grantham <james.r.grantham@gmail.com>
2020-04-11 15:18:23 +0100

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
 import './App.css';
+import ben from './images/humberBridge.JPG'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-<h1>Hello, my name is Sam</h1>
-    </div>
+    <Welcome className="App">
+      <h1>Hello, my name is Ben</h1>
+      <div className='image'>
+        <img src={ben} alt='ben' />
+      </div>
+    </Welcome>
   );
 }
 
-export default App;
+const Welcome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .image {
+    width: 80vw;
+    height: 80vh;
+    // border: 1px solid red;
+  }
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    // border: 1px solid red;
+  }
+`
