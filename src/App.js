5bec7553174b866adef177b486f043952c21a323
James Grantham <james.r.grantham@gmail.com>
2020-04-13 20:20:31 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
         <h2>Things I like</h2>
         <h5>bonfires</h5>
         <h5>holidays</h5>
-        <h5>sweets</h5>
+        <h5>seeing my dad</h5>
         <h5>Hull fair</h5>
       </div>
     </Welcome>
@@ -41,12 +41,13 @@ const Welcome = styled.div`
   h2 {
     text-align: center;
     margin: 20px;
+    color: #282c34;
   }
 
   h5 {
     margin-bottom: 40px;
     text-align: center;
-
+    color: #282c34;
   }
 
   a {
@@ -54,30 +55,31 @@ const Welcome = styled.div`
     color: orange;
     font-weight: bold;
     border-radius: 10px;
-    background-color: blue;
+    background-color: #282c34;
     padding: 10px;
-    margin: 70px 10px 0 10px;
+    margin: 70px 10px 50px 10px;
     width: 220px;
     text-align: center;
     transition: all 0.3s;
 
     &:hover {
-      background-color: purple;
+      background-color: blue;
       transform: scale(1.1);
     }
   }
 
   .list {
-    border: 8px solid blue;
+    border: 8px solid #282c34;
     border-radius: 20px;
-    margin-top: 50px;
+    margin-bottom: 50px;
     width: 350px;
   }
   
   .image {
     display: flex;
-    max-width: 60%;
+    max-width: 800px;
     max-height: 60%;
+    margin: 0 40px;
     // border: 30px solid red;
     border-radius: 50px;
     overflow: hidden;
